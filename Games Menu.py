5ed7ffde03a66eaab3eef20c81e50745ddd2d02c
Britney975 <britaniafisher50@gmail.com>
2025-04-17 13:43:00 -0500

    Update Games Menu.py

--- Games Menu.py
+++ Games Menu.py
@@ -6,17 +6,18 @@ from tkinter import messagebox
 def start_charades():
     start_guess_movie()
 
-def start_snake():
-    messagebox.showinfo("Tic-Tac-Toe", "Launching Tic-Tac-Toe game...")
+def start_match_the_card():
+    card_game_start()
 
-def start_tic_tac_toe():
-    messagebox.showinfo("Tic-Tac-Toe", "Launching Tic-Tac-Toe game...")
+def start_whack_a_mole():
+    play_whack_a_mole()
+
+def start_rock_paper_scissors():
+    play_rock_paper_scissors()
 
 def show_help():
     messagebox.showinfo("Help", "Instructions or game help goes here.")
 
-def exit_game():
-    root.destroy()
 
 # Setup main window
 root = tk.Tk()
@@ -39,15 +40,15 @@ title = tk.Label(title_bar_frame, text="Master Mind Menu", font=("Helvetica", 16
 title.pack(pady=10, fill="x")
 
 btn_charades = tk.Button(menu_frame, text="Charades", width=20, command=start_charades, bg="#FF8C00", fg="white", font=("Arial", 12, "bold")) # Dark Orange
-btn_snake = tk.Button(menu_frame, text="Number Guessing", width=20, command=start_snake, bg="#FFFF00", fg="black", font=("Arial", 12, "bold")) # Yellow
-btn_tic = tk.Button(menu_frame, text="Tic-Tac-Toe", width=20, command=start_tic_tac_toe, bg="#3CB371", fg="white", font=("Arial", 12, "bold")) # Medium Sea Green
+btn_mole = tk.Button(menu_frame, text="Whack a mole", width=20, command=start_whack_a_mole, bg="#FFFF00", fg="black", font=("Arial", 12, "bold")) # Yellow
+btn_card = tk.Button(menu_frame, text="Match the card", width=20, command=start_match_the_card, bg="#3CB371", fg="white", font=("Arial", 12, "bold")) # Medium Sea Green
+btn_RPC = tk.Button(menu_frame, text="Rock Paper Scissors", width=20, command=start_rock_paper_scissors, bg="#DC143C", fg="white", font=("Arial", 12, "bold")) # Medium Sea Green
 btn_help = tk.Button(menu_frame, text="Help", width=20, command=show_help, bg="#1E90FF", fg="white", font=("Arial", 12, "bold")) # Dodger Blue
-btn_exit = tk.Button(menu_frame, text="Exit", width=20, command=exit_game, bg="#DC143C", fg="white", font=("Arial", 12, "bold"))   # Crimson
 
 btn_charades.pack(pady=5, fill="x")
-btn_snake.pack(pady=5, fill="x")
-btn_tic.pack(pady=5, fill="x")
+btn_mole.pack(pady=5, fill="x")
+btn_card.pack(pady=5, fill="x")
+btn_RPC.pack(pady=5, fill="x")
 btn_help.pack(pady=5, fill="x")
-btn_exit.pack(pady=5, fill="x")
 
 root.mainloop()
